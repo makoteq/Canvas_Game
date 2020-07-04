d0a0719f6f39f68daced020e06c009bc59c1832f
Maciej Kotowski <makoteq@gmail.com>
2020-07-04 21:46:17 +0200

    fix

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import enemyClass from './eClass.js';
-import Bullet from './bClass.js';
+import enemyClass from "./eClass.js";
+import Bullet from "./bClass.js";
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -12,14 +12,12 @@ let gunClass = {
   w: 40,
   h: 40,
   visible: true,
-  playerX: 0,
-}
+  playerX: 0
+};
 canvas.addEventListener("mousemove", playerPosition);
 let gameStart = false;
-let instervalGame;
 let score = 0;
 let mode = "easy";
-let bigCout = Math.floor((Math.random() * 100) + 1);
 let gameidx = 0;
 let bullets = [];
 bullets.length = 5;
@@ -44,7 +42,7 @@ function modes() {
 
 canvas.onmouseover = () => {
   document.getElementById("canvas").style.cursor = "crosshair";
-}
+};
 
 function startScreen() {
   console.log("startScreen");
@@ -73,16 +71,16 @@ function startScreen() {
   ctx.fillStyle = "white";
   ctx.font = "25px Helvetica";
   ctx.fillText("select mode to begin", cw / 2, ch / 2 + 30);
-  scoreFunction()
-  document.getElementById("easy").onclick =()=>{
-    setMode("easy")
-  }
-  document.getElementById("medium").onclick =()=>{
-    setMode("medium")
-  }
-  document.getElementById("hard").onclick =()=>{
-    setMode("hard")
-  }
+  scoreFunction();
+  document.getElementById("easy").onclick = () => {
+    setMode("easy");
+  };
+  document.getElementById("medium").onclick = () => {
+    setMode("medium");
+  };
+  document.getElementById("hard").onclick = () => {
+    setMode("hard");
+  };
   canvas.onclick = () => {
     if (!gameStart) return;
     for (let i = 0; i < bullets.length; i++) {
@@ -92,37 +90,35 @@ function startScreen() {
         break;
       }
     }
-  }
+  };
   gunClass.visible = true;
-
 }
-function setMode(modee){
+function setMode(modee) {
   gunClass.visible = true;
   gameStart = true;
-  mode=modee;
-  for(let e=0;e<3;++e){
-  document.getElementsByClassName('button')[e].style.display = 'none';
+  mode = modee;
+  for (let e = 0; e < 3; ++e) {
+    document.getElementsByClassName("button")[e].style.display = "none";
   }
 }
 
-
 window.onload = () => {
-  modes()
-  startScreen()
+  modes();
+  startScreen();
   instervalGame = setInterval(game, 1000 / 60);
-}
+};
 
 function overScreen() {
-  for(let e=0;e<3;++e){
-    document.getElementsByClassName('button')[e].style.display = 'block';
-    }
+  for (let e = 0; e < 3; ++e) {
+    document.getElementsByClassName("button")[e].style.display = "block";
+  }
   gameStart = false;
   /*start screen*/
   for (let i = 0; i < enemy.length; i++) {
     console.log("speed=" + enemy[i].speed);
     enemy[i].visible = false;
-    if(enemy[i].type==2){
-    enemy[i].randomCout();
+    if (enemy[i].type == 2) {
+      enemy[i].randomCout();
     }
   }
   for (let i = 0; i < bullets.length; i++) {
@@ -144,18 +140,18 @@ function overScreen() {
   ctx.font = "25px Helvetica";
   ctx.fillStyle = "white";
   ctx.fillText("select mode to play again", cw / 2, ch / 2 + 30);
-  scoreFunction()
+  scoreFunction();
 
-  scoreFunction()
-  document.getElementById("easy").onclick =()=>{
-    setMode("easy")
-  }
-  document.getElementById("medium").onclick =()=>{
-    setMode("medium")
-  }
-  document.getElementById("hard").onclick =()=>{
-    setMode("hard")
-  }
+  scoreFunction();
+  document.getElementById("easy").onclick = () => {
+    setMode("easy");
+  };
+  document.getElementById("medium").onclick = () => {
+    setMode("medium");
+  };
+  document.getElementById("hard").onclick = () => {
+    setMode("hard");
+  };
   canvas.onclick = () => {
     if (!gameStart) return;
     for (let i = 0; i < bullets.length; i++) {
@@ -165,16 +161,12 @@ function overScreen() {
         break;
       }
     }
-  }
+  };
 }
-
-
-
 
 function table() {
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, cw, ch);
-
 }
 
 function gun() {
@@ -194,7 +186,6 @@ function playerPosition(e) {
 }
 
 function strzal() {
-
   for (let i = 0; i < bullets.length; i++) {
     if (bullets[i].y <= 0) {
       bullets[i].visible = false;
@@ -202,15 +193,23 @@ function strzal() {
       continue;
     }
     if (bullets[i].visible == true) {
-      ctx.drawImage(bullets[i].img, bullets[i].x, bullets[i].y, bullets[i].w, bullets[i].h);
+      ctx.drawImage(
+        bullets[i].img,
+        bullets[i].x,
+        bullets[i].y,
+        bullets[i].w,
+        bullets[i].h
+      );
       for (let x = 0; x < enemy.length; x++) {
         if (!enemy[x].visible) continue;
         //else if ((enemy[x].y >= bullets[i].y) && ((enemy[x].x >= bullets[i].x - (enemy[x].x+bullets[i].x)/2) && (enemy[x].x <= bullets[i].x + (enemy[x].x+bullets[i].x)/2))) {
-        if ((enemy[x].y + enemy[x].h >= bullets[i].y) &&
-          (
-            ((bullets[i].x >= enemy[x].x) && (bullets[i].x <= enemy[x].x + enemy[x].w)) ||
-            ((bullets[i].x + bullets[i].w <= enemy[x].x + enemy[x].w) && (bullets[i].x + bullets[i].w >= enemy[x].x))
-          )) {
+        if (
+          enemy[x].y + enemy[x].h >= bullets[i].y &&
+          ((bullets[i].x >= enemy[x].x &&
+            bullets[i].x <= enemy[x].x + enemy[x].w) ||
+            (bullets[i].x + bullets[i].w <= enemy[x].x + enemy[x].w &&
+              bullets[i].x + bullets[i].w >= enemy[x].x))
+        ) {
           if (enemy[x].enemyType == 1) {
             ++score;
             bullets[i].visible = false;
@@ -239,7 +238,13 @@ function strzal() {
 function meteoAnimate() {
   for (let i = 0; i < enemy.length; i++) {
     if (enemy[i].visible == true) {
-      ctx.drawImage(enemy[i].img, enemy[i].x, enemy[i].y, enemy[i].w, enemy[i].h);
+      ctx.drawImage(
+        enemy[i].img,
+        enemy[i].x,
+        enemy[i].y,
+        enemy[i].w,
+        enemy[i].h
+      );
       if (enemy[i].y + enemy[i].h >= ch - 20) {
         score = 0;
         overScreen();
@@ -264,10 +269,10 @@ function game() {
 
   ++gameidx;
   for (let x = 0; x < enemy.length; x++) {
-    if ((gameidx % enemy[x].how) == 0) {
+    if (gameidx % enemy[x].how == 0) {
       if (enemy[x].cout <= 0) {
         for (let i = 0; i < enemy.length; i++) {
-          if ((enemy[i].visible == false) && (enemy[i].enemyType == 2)) {
+          if (enemy[i].visible == false && enemy[i].enemyType == 2) {
             console.log("GENERUJE");
             enemy[i].setPosition(Math.floor(Math.random() * (450 - 50)), 20);
             enemy[i].visible = true;
@@ -276,12 +281,12 @@ function game() {
         }
       } else {
         for (let i = 0; i < enemy.length; i++) {
-          if ((enemy[i].visible == false) && (enemy[i].enemyType == 1)) {
+          if (enemy[i].visible == false && enemy[i].enemyType == 1) {
             enemy[i].setPosition(Math.floor(Math.random() * (450 - 50)), 20);
             enemy[i].visible = true;
             console.log(enemy[i].x);
             continue;
-          } else if ((enemy[i].visible == false) && (enemy[i].enemyType == 2)) {
+          } else if (enemy[i].visible == false && enemy[i].enemyType == 2) {
             enemy[x].decraseCout();
             console.log("cout=" + enemy[i].cout);
             continue;
@@ -289,16 +294,13 @@ function game() {
         }
       }
     }
-
   }
 
+  table();
+  ground();
+  strzal();
+  scoreFunction();
 
-  table()
-  ground()
-  strzal()
-  scoreFunction()
-
-  meteoAnimate()
-  gun()
-
+  meteoAnimate();
+  gun();
 }
